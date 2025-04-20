31628e26513a6810a4892c64da91b6d7102ee8ce
Lzorn <lzr18535482977@gmail.com>
2025-04-21 01:12:56 +0800

    小小的命令行解析参数的shift宏

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #define __PRINT_DEBUG_INFO__ 1
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include "Graph/GraphManager.hpp"
 using namespace Moonlight::Graph;
 
@@ -10,9 +12,13 @@ void TestGraphManager(){
     auto& graph = GraphManager<>::Instance().GetAEmptyGraph();
     graph.AddVertex();
 }
+#define shift(xs, xs_sz) ( xs_sz <= 0 ? (fprintf(stderr, "%s:%d: OUT OF BOUNDS BRUUH", __FILE__, __LINE__), abort()) : (void)0, --xs_sz, *xs++)
 
-int main(){
-    TestGraphManager();
-
+int main(int argc, char **argv){
+    // TestGraphManager();
+    while(argc > 0){
+        printf("%s\n", shift(argv, argc));
+    }
+    shift(argv, argc);
     return 0;
 }
